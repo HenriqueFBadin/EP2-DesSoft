4355a7e121a42dc983ab5374a52b32bb75233127
Henrique <henrique.badin@gmail.com>
2022-05-06 12:02:28 -0300

    Pais e dados do escolhido

--- dados_pais_escolhido.py
+++ dados_pais_escolhido.py
@@ -1,8 +1,8 @@
-def pais_escolhido(palpite):
-
-
-    dicio_final=0 #esse dicionario se refere ao conteudo de base_de_paises, mudar posteriormente seu resultado
-
-    escolha=palpite.lower
-    dados_palpite=dicio_final[escolha]
+import inicio_jogo
+import base_de_paises
+def pais_escolhido(palpite, base_organizada):
+    escolha=palpite.lower()
+    dados_palpite=base_organizada[escolha]
     return dados_palpite
+
+pd_escolhido=(pais_escolhido(inicio_jogo.palpite, base_de_paises.base_organizada))
