6e28bd72f9b7e337a94d2c8cfbbaddafbf45bf69
Ellen Coutinho <ellencls@al.insper.edu.br>
2022-05-06 12:15:14 -0300

    Haversine e pais sorteado adequado as condiçoes

---
+++ dados_pais_sorteado.py
@@ -0,0 +1,8 @@
+import sorteando_paises
+import base_de_paises
+
+def dados_sorteados(pais_sorteado, base_organizada):
+    dados_ps = base_organizada[pais_sorteado]
+    return dados_ps
+
+dados_ps = (dados_sorteados(sorteando_paises.pais_acerto, base_de_paises.base_organizada))
--- distancia_de_haversine.py
+++ distancia_de_haversine.py
@@ -1,7 +1,11 @@
 import math
+import base_de_dados
+import sorteando_paises
+import inicio_jogo
+import dados_pais_escolhido
+import dados_pais_sorteado
 
 def haversine(r, fi1, lamb1, fi2, lamb2):
-    r=6371
     f1=math.radians(fi1)
     f2=math.radians(fi2)
     l1=math.radians(lamb1)
@@ -14,3 +18,5 @@ def haversine(r, fi1, lamb1, fi2, lamb2):
     a6=math.sqrt(a2+a5)
     d=2*r*math.asin(a6)
     return d
+
+distancia = (haversine(base_de_dados.EARTH_RADIUS, dados_pais_escolhido.pd_escolhido['geo']['latitude'], dados_pais_escolhido.pd_escolhido['geo']['longitude'], dados_pais_sorteado.dados_ps['geo']['latitude'], dados_pais_sorteado.dados_ps['geo']['longitude']))
