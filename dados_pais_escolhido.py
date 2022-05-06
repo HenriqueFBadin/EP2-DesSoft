2397264bf999a928afa868715c7502461dae84da
Henrique <henrique.badin@gmail.com>
2022-05-06 11:40:41 -0300

    Função de dados do pais escolhido

---
+++ dados_pais_escolhido.py
@@ -0,0 +1,8 @@
+def pais_escolhido(palpite):
+
+
+    dicio_final=0 #esse dicionario se refere ao conteudo de base_de_paises, mudar posteriormente seu resultado
+
+    escolha=palpite.lower
+    dados_palpite=dicio_final[escolha]
+    return dados_palpite
