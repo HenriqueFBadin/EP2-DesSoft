178b5f200445032fcbf1cc775d8b93307d3dae82
Ellen Coutinho <ellencls@al.insper.edu.br>
2022-05-07 23:00:31 -0300

    As cores da bandeira se acumulam conforme pedidas

--- inicio_jogo.py
+++ inicio_jogo.py
@@ -47,14 +47,53 @@ print("    desisto    - desiste da rodada")
 print("    inventario - exibe sua posição")
 print("\nUm país foi escolhido, tente adivinhar!")
 
+#Definindo a lista de cores da bandeira
+lista_cores=[]
+cor_bandeira=(base_organizada[pais_acerto]["bandeira"])
+for cor in cor_bandeira:
+    if cor_bandeira[cor]!=0:
+        lista_cores.append(cor)
+
+
 distancias = {}
+colores = '' #Serve para acrescentar as cores pedidas nas dicas no terminal
 i = 20
+j = 0
 while i>-1:
     print('Você tem {0} tentativa(s)' .format(i))
     palpite = input('Qual o seu palpite? ')
-    
-    #Sistema de dicas para cor da bandeira funciona apenas para a 1a tentativa(resolver posteriormente):
-    if palpite.lower()=="dica" or palpite.lower()=="dicas":
+
+    if i==0:
+        print("Você perdeu! Seu país era: {}".format(pais_acerto))
+        if input("Quer jogar novamente? [s/n] ")=="s":
+            i=20
+        else:
+            i=-1    
+
+    #Inventario e desistencia:
+        #palpite.lower() == "desisto" or palpite.lower() == "inventário":
+    else:
+        if palpite in base_organizada.keys():
+            pd_escolhido=(pais_escolhido(palpite, base_organizada))
+
+        #Distância entre os dois países, o escolhido e o sorteado:
+            distancia = (distancia_de_haversine.haversine(base_de_dados.EARTH_RADIUS, pd_escolhido['geo']['latitude'], pd_escolhido['geo']['longitude'], dados_ps['geo']['latitude'], dados_ps['geo']['longitude']))
+                
+            if dados_ps==pd_escolhido:
+                print("Parabéns! Você acertou o país em {} tentativas!".format((i)))
+                
+
+            else:
+                distancias[palpite] = distancia
+                print('Distâncias: ')
+                for elemento in distancias.items():
+                    print('    {0:.3f} km -> {1}' .format(elemento[1],elemento[0]))
+                
+                print('Dicas: ')
+
+            #Sistema de dicas para cor da bandeira funciona apenas para a 1a tentativa(resolver posteriormente):
+        if palpite.lower()=="dica" or palpite.lower()=="dicas":
+
             print("Mercado de Dicas")
             print("----------------------------------------")
             print('1. Cor da bandeira  - custa 4 tentativas')
@@ -64,53 +103,35 @@ while i>-1:
             print('5. Continente       - custa 7 tentativas')
             print('0. Sem dica')
             print("----------------------------------------")
-            
-            if input("\nEscolha sua opção [0|1|2|3|4|5]: ")=="1":
-                lista_cores=[]
-                cor_bandeira=(base_organizada[pais_acerto]["bandeira"])
-                for cor in cor_bandeira:
-                    if cor_bandeira[cor]!=0:
-                        lista_cores.append(cor)
-                cor_bandeira=random.choice(lista_cores)
-                while cor_bandeira=="outras":
-                    cor_bandeira=random.choice(lista_cores)
+                    
+            opcao = input("\nEscolha sua opção [0|1|2|3|4|5]: ")
+
+            if opcao == "1":
+                cor_escolhida=random.choice(lista_cores)
+
+                while cor_escolhida=="outras":
+                    cor_escolhida=random.choice(lista_cores)
+                
+                lista_cores.remove(cor_escolhida)
+
+                if j == 0:
+                    colores = colores + cor_escolhida
+                    mensagem = '  - Cores da bandeira: {0}' .format(colores)
+                    print(mensagem)
+                    j += 1
+                
+                elif j>0:
+                    print(mensagem + ',' + ' ' + cor_escolhida)
+                    mensagem = mensagem + ',' + ' ' + cor_escolhida
+    
+
                 i-=4
-            elif input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="0" or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="1"or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="2"or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="3"or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="4"or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="5":
-                print("Opção inválida, tente novamente!")
 
-    if palpite not in base_organizada.keys():
-        palpite = input('Esse país não existe! Tente de novo.\n Qual o seu palpite? ')
+                #elif input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="0" or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="1"or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="2"or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="3"or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="4"or input("\nEscolha sua opção [0|1|2|3|4|5]: ")!="5":
+                    #print("Opção inválida, tente novamente!")
 
-    #Inventario e desistencia:
-        #palpite.lower() == "desisto" or palpite.lower() == "inventário":
-      
-    pd_escolhido=(pais_escolhido(palpite, base_organizada))
-
-    #Distância entre os dois países, o escolhido e o sorteado:
-    distancia = (distancia_de_haversine.haversine(base_de_dados.EARTH_RADIUS, pd_escolhido['geo']['latitude'], pd_escolhido['geo']['longitude'], dados_ps['geo']['latitude'], dados_ps['geo']['longitude']))
-
-    #Início do sistema do jogo. Com esse código podemos jogar o jogo normalmente, porém, ainda não possui dicas:
-    if dados_ps==pd_escolhido:
-        print("Parabéns! Você acertou o país em {} tentativas!".format((i)))
-        if input("Quer jogar novamente? [s/n]") =="s":
-            i=20
-        else:
-            i=0
-
-    if i==0:
-        print("Você perdeu! Seu país era: {}".format(pais_acerto))
-        if input("Quer jogar novamente? [s/n] ")=="s":
-            i=20
-        else:
-            i=-1
-    
-    else:
-        distancias[palpite] = distancia
-        print('Distâncias: ')
-        for elemento in distancias.items():
-            print('    {0:.3f} km -> {1}' .format(elemento[1],elemento[0]))
-        
+        elif palpite not in base_organizada.keys():
+            palpite = input('Esse país não existe! Tente de novo.\n Qual o seu palpite? ')
+            
         i-=1
-
-
-    
+        
